7be3f658b4628f7c6155faa89f0b4793ea63d197
Maksim Spirkin <dev@avtocontrol.com>
2021-08-16 18:49:33 +0300

    update: Lesson number three

--- app/src/main/java/com/avtocontrol/composelearn1/MainActivity.kt
+++ app/src/main/java/com/avtocontrol/composelearn1/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.avtocontrol.composelearn1
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,12 +17,16 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.avtocontrol.composelearn1.data.Message
+import com.avtocontrol.composelearn1.ui.theme.ComposeLearn1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            ComposeLearn1Theme() {
+                MessageCard(Message("Android", "Jetpack Compose"))
+            }
+
         }
     }
     //Определение составной функции MessageCard
@@ -28,12 +35,15 @@ class MainActivity : ComponentActivity() {
         Row(modifier = Modifier.padding(all = 8.dp)) {
             Image(
                 painter = painterResource(id = R.drawable.ic_baseline_airplay_24),
-                contentDescription = "Contacct profile picture",
+                contentDescription = "Contact profile picture",
                 modifier = Modifier
                     //Изменение размера картинки
                     .size(40.dp)
                     //Обрезать изображение по форме круга
                     .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colors.secondary, shape = CircleShape)
+
+
 
             )
             //Горизонтальный отступ между изображением и текстом
@@ -41,20 +51,35 @@ class MainActivity : ComponentActivity() {
 
 
             Column {
-                Text(text = msg.author)
+
+                Text(text = msg.author,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle2)
                 //Вертикальный отступ между author и body
                 Spacer(modifier = Modifier.height(4.dp))
-                Text(text = msg.body)
+                androidx.compose.material.Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                    Text(text = msg.body,
+                        modifier = Modifier.padding(4.dp),
+                        style = MaterialTheme.typography.body2)
+                }
+
             }
         }
 
 
     }
     //Предварительный просмотр составной функции MessageCard
-    @Preview
+    @Preview(name = "Light Mode")
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode"
+    )
     @Composable
     fun PreviewMessageCard() {
-        MessageCard(Message("Android", "Jetpack Compose"))
+        ComposeLearn1Theme() {
+            MessageCard(Message("Android", "Jetpack Compose"))
+        }
     }
 }
 
