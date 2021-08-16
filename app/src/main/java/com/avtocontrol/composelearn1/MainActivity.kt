fd43d90f9b75a8854215cab44162476ca48827d1
Maksim Spirkin <dev@avtocontrol.com>
2021-08-16 21:30:50 +0300

    update: Lesson create a list (analog RecyclerView)

--- app/src/main/java/com/avtocontrol/composelearn1/MainActivity.kt
+++ app/src/main/java/com/avtocontrol/composelearn1/MainActivity.kt
@@ -4,19 +4,26 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.avtocontrol.composelearn1.data.Message
+import com.avtocontrol.composelearn1.data.SampleData
 import com.avtocontrol.composelearn1.ui.theme.ComposeLearn1Theme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +31,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLearn1Theme() {
-                MessageCard(Message("Android", "Jetpack Compose"))
+                Conversation(messages = SampleData.conversationSample)
             }
 
         }
@@ -49,17 +56,24 @@ class MainActivity : ComponentActivity() {
             //Горизонтальный отступ между изображением и текстом
             Spacer(modifier = Modifier.width(8.dp))
 
+            var isExpanded by remember { mutableStateOf(false)}
+            val surfaceColor: Color by animateColorAsState(if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface)
 
-            Column {
+
+            Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
 
                 Text(text = msg.author,
                     color = MaterialTheme.colors.secondaryVariant,
                     style = MaterialTheme.typography.subtitle2)
                 //Вертикальный отступ между author и body
                 Spacer(modifier = Modifier.height(4.dp))
-                androidx.compose.material.Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                androidx.compose.material.Surface(shape = MaterialTheme.shapes.medium,
+                    elevation = 1.dp,
+                    color = surfaceColor,
+                    modifier = Modifier.animateContentSize().padding(1.dp)) {
                     Text(text = msg.body,
-                        modifier = Modifier.padding(4.dp),
+                        modifier = Modifier.padding(all= 4.dp),
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                         style = MaterialTheme.typography.body2)
                 }
 
@@ -67,6 +81,14 @@ class MainActivity : ComponentActivity() {
         }
 
 
+    }
+    @Composable
+    fun Conversation(messages: List<Message>) {
+       LazyColumn {
+           items(messages) {message ->
+               MessageCard(message)
+           }
+       }
     }
     //Предварительный просмотр составной функции MessageCard
     @Preview(name = "Light Mode")
@@ -78,7 +100,7 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun PreviewMessageCard() {
         ComposeLearn1Theme() {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            Conversation(SampleData.conversationSample)
         }
     }
 }
